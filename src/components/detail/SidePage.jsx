6a891c308b4716f0ea0c500ba011b06398a74e18
redberry0217 <redberry0217@gmail.com>
2024-02-24 23:42:39 +0900

    Fix: detail페이지 UI 작업중

--- src/components/detail/SidePage.jsx
+++ src/components/detail/SidePage.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SearchBar } from 'components/Mapsearch';
-import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import userImg from 'assets/defaultImg.jpg';
 
 export default function SidePage() {
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleSearch = (searchTerm) => {
     console.log(searchTerm);
   };
 
-  const GoBackClickHandler = () => {};
+  const GoBackClickHandler = () => {
+    navigate(`/`);
+  };
 
   return (
     <SidePageContainer>
       <GoBackButton onClick={GoBackClickHandler}>◀</GoBackButton>
+      <WriterBox>
+        <img src={userImg} width="40" style={{ borderRadius: '50%' }} />
+        냠냠박사
+      </WriterBox>
       <SearchBar onSearch={handleSearch} />
     </SidePageContainer>
   );
@@ -39,4 +46,29 @@ const GoBackButton = styled.div`
   padding: 15px;
   border-radius: 15px;
   cursor: pointer;
+`;
+
+const WriterBox = styled.div`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  display: flex;
+  align-items: center;
+
+  /* border: 1px solid #c70000; */
+  border-radius: 20px;
+  box-shadow: 2px 2px 5px 2px #e0c3aea2;
+
+  & h2 {
+    font-family: 'SunBatang-Medium';
+    padding: 10px;
+    height: 30px;
+    font-size: 18px;
+    color: #784b31;
+  }
+
+  & p {
+    padding: 10px;
+    color: #b6856a;
+  }
 `;
