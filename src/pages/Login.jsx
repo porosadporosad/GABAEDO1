aec120908eb87296c0baef5b0e175f749c781fd6
porosadporosad <tmxk1594@naver.com>
2024-02-24 18:15:32 +0900

    Fix:로컬스토리지 제거

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,9 +26,6 @@ export default function Login() {
       // 로그인한 유저 정보
       const { accessToken, userId, success, avatar, nickname } = response.data;
       localStorage.setItem('accessToken', JSON.stringify(accessToken));
-      // localStorage.setItem('userId', JSON.stringify(userId));
-      // localStorage.setItem('avatar', JSON.stringify(avatar));
-      // localStorage.setItem('nickname', JSON.stringify(nickname));
 
       toast.success(`${nickname}님 환영합니다!`);
       navigate('/');
