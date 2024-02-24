85fea8cfc312f355b59d05ee85837230094fe76f
redberry0217 <redberry0217@gmail.com>
2024-02-24 17:17:53 +0900

    Fix: Layout 처리

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
-function Layout() {
-  return <div>Layout</div>;
+export default function Layout() {
+  return (
+    <>
+      <main />
+      <Outlet />
+    </>
+  );
 }
-
-export default Layout;
