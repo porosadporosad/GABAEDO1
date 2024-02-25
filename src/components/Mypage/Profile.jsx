9a94792c7c054a726e18c043ced7dd108ad5710f
yeon <test2@test2.com>
2024-02-25 15:12:56 +0900

    [Profile] assets import 위치 변경

--- src/components/Mypage/Profile.jsx
+++ src/components/Mypage/Profile.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import defaultImg from '../assets/defaultImg.jpg';
+import defaultImg from '../../assets/defaultImg.jpg';
 
 export default function Profile() {
   const [isEditing, setIsEditing] = useState(false);
