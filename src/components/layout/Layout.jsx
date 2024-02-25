3efaba8acf0b32edd77569b86ef84b6051de7cc8
redberry0217 <redberry0217@gmail.com>
2024-02-25 15:02:07 +0900

    Fix: 레이아웃 라우팅 수정, 메인 검색기능 머지

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import Header from 'components/Header';
 import { Outlet } from 'react-router';
 
-export default function Layout({ children }) {
+export default function Layout() {
   return (
     <div>
       <Header />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import Banner from 'components/Banner';
-import CurationList from 'components/CurationList';
-import React from 'react';
+import React, { useState } from 'react';
+import Banner from '../components/Banner';
+import CurationList from '../components/CurationList';
 
-function Home() {
+export default function Home() {
+  const [searchKeyword, setSearchKeyword] = useState('');
+
   return (
     <div>
-      <Banner />
-      <CurationList />
+      <Banner onSearch={setSearchKeyword} />
+      <CurationList keyword={searchKeyword} />
     </div>
   );
 }
-
-export default Home;
