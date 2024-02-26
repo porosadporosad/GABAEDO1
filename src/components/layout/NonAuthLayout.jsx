3b59a9b388944700d4b8a4b31548aca8755ac4df
porosadporosad <tmxk1594@naver.com>
2024-02-26 23:03:51 +0900

    Fix:로그인확인 로컬스토리지로 변경

--- src/components/layout/NonAuthLayout.jsx
+++ src/components/layout/NonAuthLayout.jsx
@@ -1,12 +1,13 @@
-import { useCurrentUser } from 'shared/database';
+// import { useCurrentUser } from 'shared/database';
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import Header from 'components/Header';
 
 export default function NonAuthLayout() {
   // const { data } = useQuery('users', getUsers);
-  const { data } = useCurrentUser();
-  const isLoggedin = data;
+  // const { data } = useCurrentUser();
+  const userId = JSON.parse(localStorage.getItem('userId'));
+  const isLoggedin = userId;
   if (isLoggedin) {
     alert(`이미 로그인된 상태입니다.`);
     console.log(`이미 로그인됨`);
