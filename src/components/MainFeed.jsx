9fbbbd375ed9d9d51db78e1e29132e9c17102f94
kimsohyeon <aotoyae@gmail.com>
2024-02-27 19:55:14 +0900

    Add: 메뉴별로 포스트 보기

--- src/components/MainFeed.jsx
+++ src/components/MainFeed.jsx
@@ -1,26 +1,24 @@
 import styled from 'styled-components';
 import { getPosts, useCurrentUser } from 'shared/database';
 import { useQuery } from 'react-query';
-import { useNavigate } from 'react-router';
 import { useState } from 'react';
 import CreatePost from './CreatePost';
 import { hashtageData } from 'shared/hashtageData';
+import PostsList from './PostsList';
 
-export default function PostList({ keyword }) {
+export default function MainFeed({ keyword }) {
   const { data: loginUserData } = useCurrentUser();
   const { isLoading, data } = useQuery('posts', getPosts);
-  const navigate = useNavigate();
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [menu, setMenu] = useState('');
 
   if (isLoading) {
     return <h1>Loading</h1>;
   }
 
-  const filteredData = data.filter((post) => post.title.includes(keyword) || post.content.includes(keyword));
+  // const searchedData = data.filter((post) => post.title.includes(keyword) || post.content.includes(keyword));
 
-  const boxClickHandler = (id) => {
-    navigate(`detail/${id}`);
-  };
+  const filteredData = menu ? data.filter((post) => post.hashtag.includes(menu)) : data;
 
   return (
     <>
@@ -28,12 +26,6 @@ export default function PostList({ keyword }) {
         <CreatePost modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />
       </AddPostModal>
       <Article>
-        <ListTitle>태그별 가배도</ListTitle>
-        <HashtagNav>
-          {hashtageData.map((item) => (
-            <button key={item}>{item}</button>
-          ))}
-        </HashtagNav>
         <PostListHeader>
           <ListTitle>가배도 모아보기</ListTitle>
           <CreatePostBtn
@@ -45,19 +37,30 @@ export default function PostList({ keyword }) {
             +
           </CreatePostBtn>
         </PostListHeader>
-        <AllSection>
-          {filteredData.map((post) => (
-            <PostListBox key={post.id} onClick={() => boxClickHandler(post.id)}>
-              <h2>{post.title}</h2>
-              <h3>{post.content}</h3>
-              <HashtagList>
-                {post.hashtag.map((item) => (
-                  <p key={item}>{item}</p>
-                ))}
-              </HashtagList>
-            </PostListBox>
+        <HashtagMenu>
+          <button
+            onClick={() => {
+              setMenu('');
+            }}
+          >
+            📔 가배도 전체보기
+          </button>
+          {hashtageData.map((item) => (
+            <button
+              key={item}
+              onClick={() => {
+                setMenu(item);
+              }}
+            >
+              {item}
+            </button>
           ))}
-        </AllSection>
+        </HashtagMenu>
+        <PostsList postsData={filteredData} />
+        <ListTitle>카페 모아보기</ListTitle>
+        {/* <PostsList searchedData={searchedData} /> */}
+        <ListTitle>가배도 전체보기</ListTitle>
+        <PostsList postsData={data} />
       </Article>
     </>
   );
@@ -109,71 +112,7 @@ const CreatePostBtn = styled.button`
   cursor: pointer;
 `;
 
-const AllSection = styled.section`
-  height: 280px;
-  padding: 0 10px;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  overflow-y: scroll;
-
-  &::-webkit-scrollbar {
-    width: 7px;
-  }
-  &::-webkit-scrollbar-thumb {
-    background-color: #e0c3ae;
-    border-radius: 5px;
-    /* box-shadow: inset 0px 0px 5px #fff; */
-  }
-  &::-webkit-scrollbar-track {
-    background-color: #fff9f3;
-    border-radius: 5px;
-  }
-`;
-
-const PostListBox = styled.div`
-  width: 380px;
-  height: 120px;
-  padding: 10px;
-  margin: 10px 0;
-  text-align: center;
-  cursor: pointer;
-
-  /* border: 1px solid #c70000; */
-  border-radius: 20px;
-  box-shadow: 2px 2px 5px 2px #e0c3aea2;
-
-  & h2 {
-    font-family: 'SunBatang-Medium';
-    padding: 10px;
-    height: 30px;
-    font-size: 18px;
-    color: #784b31;
-  }
-
-  & h3 {
-    padding: 10px;
-    color: #b6856a;
-  }
-`;
-
-const HashtagList = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 4px;
-  margin: 5px 0;
-
-  & p {
-    padding: 2px 10px;
-
-    color: #b6856a;
-    background-color: #fff9f3;
-    /* border: 1px solid #e0c3ae7f; */
-    border-radius: 20px;
-    font-size: 12px;
-  }
-`;
-
-const HashtagNav = styled.nav`
+const HashtagMenu = styled.nav`
   height: 50px;
   line-height: 50px;
   margin: 10px 0;
